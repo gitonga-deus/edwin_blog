ee0aab6b46dde5d317ed63814f93a4e1d77d377f
gitonga <gitonga.deus@gmail.com>
2022-11-11 23:01:12 +0300

    Carousel

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import img1 from "../../assets/img1.jpg"
+import img2 from "../../assets/img2.jpg"
+import img3 from "../../assets/img3.jpg"
+
+const CarouselItem = () => {
+	const [index, setIndex] = useState(0);
+
+	const handleSelect = (selectedIndex, e) => {
+		setIndex(selectedIndex);
+	};
+
+	const renderCarousel = () => {
+		let result = [];
+		
+	}
+
+	return (
+		<Carousel activeIndex={index} onSelect={handleSelect} fade>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src={img1}
+					style={{ borderRadius: "10px", }}
+					height="450px"
+					alt="First slide"
+				/>
+				<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src={img2}
+					style={{ borderRadius: "10px", }}
+					height="450px"
+					alt="Second slide"
+				/>
+
+				<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src={img3}
+					style={{ borderRadius: "10px", }}
+					height="450px"
+					alt="Third slide"
+				/>
+
+				<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
+}
+
+export default CarouselItem;
