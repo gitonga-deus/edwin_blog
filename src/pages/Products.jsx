1186df8f0f7dd7b501bdadca1594914c7030ad68
gitonga <gitonga.deus@gmail.com>
2022-11-13 23:00:02 +0300

    Contact, Event and Product Pages

---
+++ src/pages/Products.jsx
@@ -0,0 +1,20 @@
+import { Row } from "react-bootstrap"
+
+const ProductItem = () => {
+	return (
+		<div>
+			<h1>Product Item</h1>
+		</div>
+	)
+}
+
+const Products = () => {
+	return (
+		<div>
+			<h1>Products</h1>
+			<ProductItem />
+		</div>
+	)
+}
+
+export default Products;
