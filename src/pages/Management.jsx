79441d31aafca7b932477d74259a1bdf541c3bd3
gitonga <gitonga.deus@gmail.com>
2022-11-17 00:37:34 +0300

    minor changes

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -16,11 +16,11 @@ const Person = ({ person }) => {
 					<CgProfile fontSize={60} style={{ height: "150px" }} />
 				</Col>
 				<Col sm={5}>
-					<h5>{ }</h5>
-					<span>{ }</span>
-					<span>{ }</span>
-					<span>{ }</span>
-					<p>{ }</p>
+					<h5>Name: {person.name}</h5>
+					<p>Rank: {person.rank}</p>
+					<p>Email Address: {person.emailAddress}</p>
+					<p>Mobile Number: {person.phone_no}</p>
+					<p>{person.description}</p>
 				</Col>
 			</Card>
 		</Col>
@@ -28,9 +28,16 @@ const Person = ({ person }) => {
 }
 
 const Management = () => {
-	const renderPersons = () => {
+	const { management } = data;
 
-	}
+	const renderPersons = () => {
+		let result = [];
+		management.map((person, i) => {
+			result.push(<Person person={person} key={i} />);
+		});
+		return result;
+	};
+
 	return (
 		<Row>
 			<PageTitle title="Meet the Team" />
@@ -63,10 +70,11 @@ const Management = () => {
 				</p>
 			</div>
 			<PageTitle title="The Management Committee" />
-
+			{renderPersons()}
+			{/* 
 			<Person name="John Doe" rank="Patron" />
-			<Person name="Jane Doe" rank="Chairperson" />
-			{/* <Person name="Jane Doe" rank="Secretary" />
+			<Person name="Jane Doe" rank="Chairperson" /> 
+			<Person name="Jane Doe" rank="Secretary" />
 			<Person name="John Smith" rank="Treasurer" /> */}
 		</Row>
 	);
