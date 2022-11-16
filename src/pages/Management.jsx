58f601b3290e71137eb18954cd7e46d11f97a14b
gitonga <gitonga.deus@gmail.com>
2022-11-17 00:00:17 +0300

    Updated Management Page

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -6,25 +6,34 @@ import { CgProfile } from "react-icons/cg"
 
 import hero from "../../assets/carousel_1.jpg";
 
-const Person = ({ name, rank }) => {
+import data from "../data.json";
+
+const Person = ({ person }) => {
 	return (
-		<Col className="my-4 text-center" sm={3}>
+		<Col className="my-4" sm={8}>
 			<Card>
-				<Card.Body>
+				<Col sm={3}>
 					<CgProfile fontSize={60} style={{ height: "150px" }} />
-					<Card.Title><h4>{name}</h4></Card.Title>
-					<Card.Text>{rank}</Card.Text>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, nulla!</p>
-				</Card.Body>
+				</Col>
+				<Col sm={5}>
+					<h5>{ }</h5>
+					<span>{ }</span>
+					<span>{ }</span>
+					<span>{ }</span>
+					<p>{ }</p>
+				</Col>
 			</Card>
 		</Col>
 	);
 }
 
 const Management = () => {
+	const renderPersons = () => {
+
+	}
 	return (
 		<Row>
-			<PageTitle title="Management and Staff" />
+			<PageTitle title="Meet the Team" />
 			<div className="py-4">
 				<img
 					className="d-block w-100"
@@ -34,13 +43,31 @@ const Management = () => {
 					alt="group photo"
 				/>
 			</div>
-			<div style={{ paddingTop: "20px" }} />
-			<PageTitle title="Meet the team" />
+			<PageTitle title="Leadership and Governance" />
+			<div className="py-4">
+				<p>
+					Leadership is supervised and regulated by <strong>Caritas Nairobi Social Promotion,</strong> an economic arm of Archdiocese of Nairobi. Management Committee comprises of seven members elected from the general membership during Annual General Meeting.
+				</p>
+				<p>
+					For effective management, the committee is structured into subcommittees which are the engines of business development. They include: {["Executive,", "Finance and Budget,", "Risk Audit and Compliance, and", "Capacity Building"].map((item, i) => (
+						<ul className="px-4 m-2">
+							<li>{item}</li>
+						</ul>
+					))}
+				</p>
+				<p>
+					<span>
+						<strong>OPERATIONS: </strong>
+					</span>
+					The group has an accountant who serves the members on daily basis in the physical office within the Parish compound. Services are fully automated with real time alert
+				</p>
+			</div>
+			<PageTitle title="The Management Committee" />
 
 			<Person name="John Doe" rank="Patron" />
 			<Person name="Jane Doe" rank="Chairperson" />
-			<Person name="Jane Doe" rank="Secretary" />
-			<Person name="John Smith" rank="Treasurer" />
+			{/* <Person name="Jane Doe" rank="Secretary" />
+			<Person name="John Smith" rank="Treasurer" /> */}
 		</Row>
 	);
 };
