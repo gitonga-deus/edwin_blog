bcec02bd768a8525e709b2d8674171d0e8a90576
gitonga <gitonga.deus@gmail.com>
2022-11-23 13:06:18 +0300

    updated the events page and other minor stuff

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = () => {
 						<Nav.Link to="/" as={NavLink}>Home</Nav.Link>
 						<Nav.Link to="/management" as={NavLink}>Management</Nav.Link>
 						<Nav.Link to="/products" as={NavLink}>Products</Nav.Link>
-						<Nav.Link to="/events" as={NavLink}>Events</Nav.Link>
+						<Nav.Link to="/event" as={NavLink}>Events</Nav.Link>
 						<Nav.Link to="/contact" as={NavLink}>Contact Us</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
