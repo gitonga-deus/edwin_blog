27131f4facd159975489e65bd8690ac948418ae2
gitonga <gitonga.deus@gmail.com>
2022-11-29 00:21:04 +0300

    fixes

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,8 +1,9 @@
-import { Row } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 
 import { PageTitle } from "../components";
 
 import hero from "../../assets/img1.jpg";
+import placeholder from "../../assets/placeholder.jpg"
 
 const Management = () => {
 
@@ -38,7 +39,20 @@ const Management = () => {
 					The group has an accountant who serves the members on daily basis in the physical office within the Parish compound. Services are fully automated with real time alert
 				</p>
 			</div>
-		</Row>
+
+			{/* <Col sm={4} lg={4} md={4}>
+				<img src={placeholder} alt="Person" height="250px" width="auto" />
+			</Col>
+			<Col sm={8} md={8} lg={8}>
+				<h3>John Doe</h3>
+				<h6>Chairman</h6>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos odit iusto saepe. Rem sunt expedita iure, earum delectus commodi.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos odit iusto saepe. Rem sunt expedita iure, earum delectus commodi.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos odit iusto saepe. Rem sunt expedita iure, earum delectus commodi.
+				</p>
+			</Col> */}
+		</Row >
 	);
 };
 
