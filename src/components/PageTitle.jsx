d5fdc019def6268bdce8abd34ad009735638c508
gitonga <gitonga.deus@gmail.com>
2022-11-28 18:23:45 +0300

    minor changes

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 
 const PageTitle = ({ title }) => {
 	return (
-		<Col sm={12} className="p-1 text-left w-100 text-center" style={{
+		<Col sm={12} className="p-1 w-100 text-center" style={{
 			marginLeft: "5px"
 		}}>
 			<h2>{title}</h2>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
 	});
 
 	const sendEmail = (e) => {
-		// e.preventDefault();
+		e.preventDefault();
 		send(
 			"service_erwc4km",
 			"template_gzb4ssv",
@@ -46,92 +46,94 @@ const Contact = () => {
 	const { location, address, emailAddress, phoneNumber } = contacts
 
 	return (
-		<div className="py-4">
+		<>
 			<PageTitle title="Get in Touch" />
-			<Row style={{
-				margin: "5px",
-				border: "1px solid #661111",
-				borderRadius: "10px"
-			}}>
-				<Col sm={5} md={5} lg={4} style={{
-					color: "#fff",
-					backgroundColor: "#661111",
-					borderRadius: "10px 0 0 10px"
+			<div className="py-3">
+				<Row style={{
+					margin: "5px",
+					border: "1px solid #661111",
+					borderRadius: "5px"
 				}}>
-					<div className="p-3">
-						<h3 className="d-flex align-items-center">
-							<BsChatDots />
-							<span style={{ marginLeft: "10px" }}>Chat to Us</span>
-						</h3>
-						<p>
-							Our friendly team is here to help: <br />
-							{emailAddress}
-						</p>
-					</div>
-					<div className="p-3">
-						<h3 className="d-flex align-items-center">
-							<SlLocationPin /> <span style={{ marginLeft: "10px" }} >Office</span>
-						</h3>
-						<p>We are located at: <br /> {address}, <br /> {location}. </p>
-					</div>
-					<div className="p-3">
-						<h3 className="d-flex align-items-center">
-							<BsTelephone />
-							<span style={{ marginLeft: "10px" }} >Phone</span>
-						</h3>
-						<p>Mon - Fri from 9am to 5pm <br /> {phoneNumber}</p>
-					</div>
-				</Col>
-				<Col sm={7} md={7} lg={8}>
-					<Form onSubmit={sendEmail} className="py-4">
-						<Row>
-							<Col sm={6} md={6}>
-								<Form.Group className="mb-2 py-1" controlId="formBasicFullName">
-									<Form.Label>Full Name</Form.Label>
-									<Form.Control
-										type="text" name="sender_name" value={toSend.sender_name}
-										placeholder="Full Name" required onChange={handleChange}
-									/>
-								</Form.Group>
-							</Col>
-							<Col sm={6}>
-								<Form.Group className="mb-2 py-1" controlId="formBasicPhoneNumber">
-									<Form.Label>Mobile Number</Form.Label>
-									<Form.Control
-										type="text" name="sender_mobile" value={toSend.sender_mobile}
-										placeholder="Mobile Number" required onChange={handleChange}
-									/>
-								</Form.Group>
-							</Col>
-						</Row>
-						<Form.Group className="mb-2 py-1" controlId="formBasicEmail">
-							<Form.Label>Email address</Form.Label>
-							<Form.Control
-								type="email" name="sender_email" value={toSend.sender_email}
-								placeholder="Email" required onChange={handleChange}
-							/>
-							<Form.Text className="text-muted">
-								We'll never share your email with anyone else.
-							</Form.Text>
-						</Form.Group>
-						<Form.Group className="mb-2" controlId="formBasicTextarea">
-							<Form.Label>Message</Form.Label>
-							<Form.Control
-								as="textarea" rows={3}
-								name="message" value={toSend.message}
-								placeholder="Message" required onChange={handleChange}
-							/>
-						</Form.Group>
-						<Button className="mb-2" type="submit" style={{
-							backgroundColor: "#661111",
-							border: "none"
-						}}>
-							Send Message
-						</Button>
-					</Form>
-				</Col>
-			</Row>
-		</div>
+					<Col sm={5} md={5} lg={4} style={{
+						color: "#fff",
+						backgroundColor: "#661111",
+						borderRadius: "5px 0 0 5px"
+					}}>
+						<div className="p-3">
+							<h3 className="d-flex align-items-center">
+								<BsChatDots />
+								<span style={{ marginLeft: "10px" }}>Chat to Us</span>
+							</h3>
+							<p>
+								Our friendly team is here to help: <br />
+								{emailAddress}
+							</p>
+						</div>
+						<div className="p-3">
+							<h3 className="d-flex align-items-center">
+								<SlLocationPin /> <span style={{ marginLeft: "10px" }} >Office</span>
+							</h3>
+							<p>We are located at: <br /> {address}, <br /> {location}. </p>
+						</div>
+						<div className="p-3">
+							<h3 className="d-flex align-items-center">
+								<BsTelephone />
+								<span style={{ marginLeft: "10px" }} >Phone</span>
+							</h3>
+							<p>Mon - Fri from 9am to 5pm <br /> {phoneNumber}</p>
+						</div>
+					</Col>
+					<Col sm={7} md={7} lg={8}>
+						<Form onSubmit={sendEmail} className="py-4">
+							<Row>
+								<Col sm={6} md={6}>
+									<Form.Group className="mb-2 py-1" controlId="formBasicFullName">
+										<Form.Label>Full Name</Form.Label>
+										<Form.Control
+											type="text" name="sender_name" value={toSend.sender_name}
+											placeholder="Full Name" required onChange={handleChange}
+										/>
+									</Form.Group>
+								</Col>
+								<Col sm={6}>
+									<Form.Group className="mb-2 py-1" controlId="formBasicPhoneNumber">
+										<Form.Label>Mobile Number</Form.Label>
+										<Form.Control
+											type="text" name="sender_mobile" value={toSend.sender_mobile}
+											placeholder="Mobile Number" required onChange={handleChange}
+										/>
+									</Form.Group>
+								</Col>
+							</Row>
+							<Form.Group className="mb-2 py-1" controlId="formBasicEmail">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control
+									type="email" name="sender_email" value={toSend.sender_email}
+									placeholder="Email" required onChange={handleChange}
+								/>
+								<Form.Text className="text-muted">
+									We'll never share your email with anyone else.
+								</Form.Text>
+							</Form.Group>
+							<Form.Group className="mb-2" controlId="formBasicTextarea">
+								<Form.Label>Message</Form.Label>
+								<Form.Control
+									as="textarea" rows={4}
+									name="message" value={toSend.message}
+									placeholder="Message" required onChange={handleChange}
+								/>
+							</Form.Group>
+							<Button className="mb-2" type="submit" style={{
+								backgroundColor: "#661111",
+								border: "none"
+							}}>
+								Send Message
+							</Button>
+						</Form>
+					</Col>
+				</Row>
+			</div>
+		</>
 	)
 }
 
