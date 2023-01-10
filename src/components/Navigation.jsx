a2b1399ac60f983d5ea96fe1d9692662476cd358
gitonga <gitonga.deus@gmail.com>
2023-01-10 15:58:22 +0300

    minor fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -16,6 +16,7 @@ const Navigation = () => {
 					<Nav className="justify-content-end" style={{ width: "100%" }}>
 						<Nav.Link to="/" as={NavLink}>Home</Nav.Link>
 						<Nav.Link to="/management" as={NavLink}>Management</Nav.Link>
+						<Nav.Link to="/event" as={NavLink}>Events</Nav.Link>
 						<Nav.Link to="/products" as={NavLink}>Products</Nav.Link>
 						<Nav.Link to="/contact" as={NavLink}>Contact Us</Nav.Link>
 					</Nav>
